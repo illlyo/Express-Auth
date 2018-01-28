2147dceca7faac387443604a5d3157dde19c10ba
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-28 11:54:44 -0500

    set up user migration file

---
+++ db/migrations/usermigrations.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users(
+	id SERIAL PRIMARY KEY,
+	username VARCHAR UNIQUE NOT NULL,
+	password_digest TEXT NOT NULL,
+	email VARCHAR UNIQUE NOT NULL,
+	name VARCHAR
+);
+
+-- Below lines allow user_id value to be brought in to other tables
+-- Replace "table_name" with the actual table name you are manipulating
+-- ALTER TABLE table_name
+-- ADD COLUMN user_id INTEGER REFERENCES users(id);
